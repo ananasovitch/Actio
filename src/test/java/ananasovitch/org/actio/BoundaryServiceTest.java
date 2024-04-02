167d79a65fb091953468c9a807efbbbef1843d81
Aleksey Vetrov <vetrovalexey28@gmail.com>
2024-04-02 17:27:09 +0300

    билд, упавший из-за недостаточного покрытия

--- src/test/java/ananasovitch/org/actio/BoundaryServiceTest.java
+++ src/test/java/ananasovitch/org/actio/BoundaryServiceTest.java
@@ -2,6 +2,7 @@ package ananasovitch.org.actio;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,26 +16,22 @@ public class BoundaryServiceTest {
 
     private final BoundaryService boundaryService = new BoundaryService();
 
-
     static Stream<Arguments> provideData() {
         return Stream.of(
                 arguments(new int[]{1, 2, 3, 4, 5}, 1),
                 arguments(new int[]{10, 20, 30, 40, 50}, 10),
-                arguments(new int[]{100, 200, 300, 400, 500}, 100),
-                arguments(new int[]{100, 5, 300, 20, 500}, 5),
+                arguments(new int[]{100, 200, 300, 400, 500}, 100)
+                /*arguments(new int[]{100, 5, 300, 20, 500}, 5),
                 arguments(new int[]{-10, -20, -3, -40, -5}, -40),
                 arguments(new int[]{10, 20, 30, 40, 5}, 5),
                 arguments(new int[]{1, 2, 3, 4, 5}, 1),
-                arguments(new int[]{0, 0, 0, 0, 0}, 0)
+                arguments(new int[]{0, 0, 0, 0, 0}, 0)*/
         );
     }
 
     @ParameterizedTest(name = "Test {index}: Минимальный элемент массива {0} равен {1}")
     @MethodSource("provideData")
     void testFindMin(int[] marks, int expectedMin) {
-        if (marks[0] == 1) { // Намеренный провал теста для массива {1, 2, 3, 4, 5}
-            fail("Намеренная ошибка - тест провален для массива " + printArray(marks));
-        }
         int min = boundaryService.findMin(marks);
         assertEquals(expectedMin, min, "Минимальное значение неверно для массива " + printArray(marks));
     }
